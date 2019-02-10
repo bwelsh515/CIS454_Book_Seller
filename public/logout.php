7e0cef6dfa0ddc198d3e4bdf15b373ca204e5dc1
jfeng07 <46981213+jfeng07@users.noreply.github.com>
2019-02-10 14:03:46 -0500

    Update logout.php

--- public/logout.php
+++ public/logout.php
@@ -5,6 +5,12 @@
     *  Logout user
     *
  -->
+ <?php
+session_start();
+if(session_destroy()) {
+      header("Location: login.php");
+   }
+?>
 Logout Page
 
 <?php require "templates/footer.php";?>
