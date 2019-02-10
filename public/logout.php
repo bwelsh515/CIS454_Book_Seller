03b1c751473adc272bbcec8a4833f60ce5f8a8ea
Brian Welsh <bwelsh@syr.edu>
2019-02-10 15:56:30 -0500

    Update logout.php

--- public/logout.php
+++ public/logout.php
@@ -1,3 +1,18 @@
+<?php
+// Initialize the session
+session_start();
+
+// Unset all of the session variables
+$_SESSION = array();
+
+// Destroy the session.
+session_destroy();
+
+// Redirect to login page
+header("location: login.php");
+exit;
+?>
+
 <?php require "templates/header.php";?>
 <!--
     *
@@ -5,12 +20,6 @@
     *  Logout user
     *
  -->
- <?php
-session_start();
-if(session_destroy()) {
-      header("Location: login.php");
-   }
-?>
 Logout Page
 
 <?php require "templates/footer.php";?>
