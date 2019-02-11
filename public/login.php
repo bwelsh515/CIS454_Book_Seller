11f03d71511644fe7505e6b02da1f013b32f8a11
Brian Welsh <bwelsh@syr.edu>
2019-02-11 16:07:53 -0500

    Update login.php

--- public/login.php
+++ public/login.php
@@ -14,7 +14,9 @@ if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: index.php");
     exit;
 }
-$error ="";
+
+$error = "";
+
 if (isset($_POST['submit'])) {
     require "../config.php";
     require "../common.php";
@@ -22,36 +24,51 @@ if (isset($_POST['submit'])) {
     try {
         $connection = new PDO($dsn, $db_username, $db_password, $db_options);
 
-        $username = $_POST['username'];
-        $password = $_POST['password'];
+        // Check if username or password fields are empty
+        if (empty(trim($_POST['username']))) {
+            $error = "Please enter a username";
+        } else {
+            $username = trim($_POST['username']);
+        }
 
+        if (empty(trim($_POST['password']))) {
+            $error = "Please enter a password";
+        } else {
+            $password = trim($_POST['password']);
+        }
+
+        // Search DB for username and password
         $sql = "SELECT * FROM user_info WHERE user_name = :username AND user_password = :password";
 
-        if ($stmt = $connection->prepare($sql)) {
-            $stmt->bindParam(":username", $username);
-            $stmt->bindParam(":password", $password);
+        // If no errors have occurred, execute the query
+        if (empty($error)) {
+            if ($stmt = $connection->prepare($sql)) {
+                $stmt->bindParam(":username", $username);
+                $stmt->bindParam(":password", $password);
 
-            if ($stmt->execute()) {
-                if ($stmt->rowCount() == 1) {
-                    if ($row = $stmt->fetch()) {
-                        $user_id = $row["user_id"];
+                if ($stmt->execute()) {
+                    if ($stmt->rowCount() == 1) {
+                        if ($row = $stmt->fetch()) {
+                            $user_id = $row["user_id"];
+                        }
+                        // Store the user information into a session on success
+                        $_SESSION["loggedin"] = true;
+                        $_SESSION["id"] = $id;
+                        $_SESSION["username"] = $username;
+
+                        // Redirect user to welcome page
+                        header("location: index.php");
                     }
-
-                    $_SESSION["loggedin"] = true;
-                    $_SESSION["id"] = $id;
-                    $_SESSION["username"] = $username;
-
-                    // Redirect user to welcome page
-                    header("location: index.php");
                 }
+                $error = "Invalid Username or Password";
             }
-            $error = "Invalid Username or Password";
+            // Close statement
+            unset($stmt);
         }
 
     } catch (PDOException $error) {
         echo $sql . "<br>" . $error->getMessage();
     }
-
 }
 ?>
 
@@ -61,22 +78,16 @@ if (isset($_POST['submit'])) {
  <div align = "center">
          <div style = "width:300px; border: solid 1px #FFD700; " align = "left">
             <div style = "background-color:#FFD700; color:#FFFFFF; padding:3px;"><b>Login</b></div>
-
             <div style = "margin:30px">
-
                <form action = "" method = "post">
                   <label>UserName  :</label><input type = "text" name = "username" class = "box"/><br /><br />
                   <label>Password  :</label><input type = "password" name = "password" class = "box" /><br/><br />
-                  <input type = "submit" name="submit" value="Submit"class="btn btn-primary"/>
+                  <input type = "submit" name="submit" value="Submit" class="btn btn-primary"/>
                   <a href="register.php" class="btn btn-success"> Register</a>
                </form>
-
                <div style = "font-size:11px; color:#cc0000; margin-top:10px"><?php echo $error; ?></div>
-
             </div>
-
          </div>
-
       </div>
 
 <?php require "templates/footer.php";?>
