e29d191deddd0f04c097527136d776c8bb69e0dc
Brian Welsh <bwelsh@syr.edu>
2019-02-11 18:03:39 -0500

    Added payment functionality

--- public/buy.php
+++ public/buy.php
@@ -41,6 +41,8 @@ try {
     echo $sql . "<br>" . $error->getMessage();
 }
 if (isset($_POST['submit'])) {
+    // get card type
+    $cardtype = $_POST['cardtype'];
     try {
         $connection = new PDO($dsn, $db_username, $db_password, $db_options);
         $sql = "UPDATE bookinfo SET is_available = 'Shipped' WHERE book_id = :id";
@@ -65,20 +67,60 @@ if (isset($_POST['submit'])) {
             <a href="index.php" class="btn btn-success pull-right">Back To Textbooks</a>
         </div>
     </div>
+<!-- Display payment form if the submit button has not been pressed -->
 <?php if (!$purchased) {?>
     <div class="wrapper">
         <form method="post">
-            <p>Are you sure you want to purchase <strong><?php echo $book_name; ?></strong> for <strong>$<?php echo $book_price; ?></strong>?</p>
-            <div class="btn-toolbar">
-                <a href="index.php" class="btn btn-danger pull-right">No</a>
-                <input type="submit" name="submit" class="btn btn-primary btn-md pull-right" value="Yes">
+            <blockquote>
+            <p>If you would like to purchase <strong><?php echo $book_name; ?></strong> for <strong>$<?php echo $book_price; ?></strong>,</p>
+            <p>please fill in your payment information below.</p>
+            </blockquote>
+
+            <div class="form-row">
+                <div class="form-group col-xs-12 required">
+                    <label for="exampleFormControlSelect1">Please Select a Card Type</label>
+                    <select class="form-control" id="cardtype" name="cardtype">
+                        <option>Mastercard</option>
+                        <option>Visa</option>
+                        <option>Discover</option>
+                    </select>
+                </div>
             </div>
+            <div class='form-row'>
+              <div class='col-xs-12 form-group required'>
+                <label class='control-label'>Name on Card</label>
+                <input class='form-control' size='4' type='text'>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='col-xs-12 form-group card required'>
+                <label class='control-label'>Card Number</label>
+                <input autocomplete='off' class='form-control card-number' size='20' type='text'>
+              </div>
+            </div>
+            <div class='form-row'>
+              <div class='col-xs-4 form-group cvc required'>
+                <label class='control-label'>CVC</label>
+                <input autocomplete='off' class='form-control card-cvc' placeholder='ex. 311' size='4' type='text'>
+              </div>
+              <div class='col-xs-4 form-group expiration required'>
+                <label class='control-label'>Expiration</label>
+                <input class='form-control card-expiry-month' placeholder='MM' size='2' type='text'>
+              </div>
+              <div class='col-xs-4 form-group expiration required'>
+                <label class='control-label'> </label>
+                <input class='form-control card-expiry-year' placeholder='YYYY' size='4' type='text'>
+              </div>
+            </div>
+
+            <input type="submit" name="submit" class="btn btn-primary btn-md pull-right" value="Purchase Textbook">
         </form>
 
     </div>
+<!-- Display success message after purchase -->
 <?php } else {?>
 <!-- TODO: Add User Location into Success Message -->
-<h3>You have successfully purchased the textbook. The textbook is being sent to your address.</h3>
+<blockquote>You have successfully purchased the textbook with your <strong><?php echo $cardtype ?></strong>. The textbook is being sent to your address.</blockquote>
 </div>
 <?php }?>
 
