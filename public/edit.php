87aa31a858b2d71a2293b08f65d46c5b43b991ac
CZJ24 <43864519+CZJ24@users.noreply.github.com>
2019-02-11 19:46:55 -0500

    Add files via upload

---
+++ public/edit.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ * Use an HTML form to edit an entry in the
+ * users table.
+ *
+ */
+
+session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
+    header("location: login.php");
+    exit;
+}
+
+
+require "../config.php";
+require "../common.php";
+
+
+
+if (isset($_POST['submit'])) {
+  try {
+    $connection = new PDO($dsn, $db_username, $db_password, $db_options);
+    $book =[
+      "id"        => $_POST['id'],
+      "book_genre" => $_POST['book_genre'],
+      "book_name"  => $_POST['book_name'],
+      "book_author"     => $_POST['book_author'],
+      "book_price"       => $_POST['book_price'],
+    ];
+
+    $sql = "UPDATE bookinfo 
+            SET  book_genre = :book_genre, 
+              book_name = :book_name, 
+              book_author = :book_author, 
+              book_price = :book_price
+            WHERE id = :id";
+  
+  $statement = $connection->prepare($sql);
+  $statement->execute($book);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+}
+  
+if (isset($_GET['id'])) {
+  try {
+    $connection = new PDO($dsn, $db_username, $db_password, $db_options);
+    $id = $_GET['id'];
+    $sql = "SELECT * FROM bookinfo WHERE id = :id";
+    $statement = $connection->prepare($sql);
+    $statement->bindValue(':id', $id);
+    $statement->execute();
+    
+    $user = $statement->fetch(PDO::FETCH_ASSOC);
+  } catch(PDOException $error) {
+      echo $sql . "<br>" . $error->getMessage();
+  }
+} else {
+    echo "Something went wrong!";
+    exit;
+}
+?>
+
+<?php require "templates/header.php"; ?>
+
+<?php if (isset($_POST['submit']) && $statement) : ?>
+	<blockquote><?php echo escape($_POST['book_name']); ?> successfully updated.</blockquote>
+<?php endif; ?>
+
+<h2>Edit the book</h2>
+
+<form method="post">
+    <?php foreach ($book as $key => $value) : ?>
+      <label for="<?php echo $key; ?>"><?php echo ucfirst($key); ?></label>
+	    <input type="text" name="<?php echo $key; ?>" id="<?php echo $key; ?>" value="<?php echo escape($value); ?>" <?php echo ($key === 'id' ? 'readonly' : null); ?>>
+    <?php endforeach; ?> 
+    <input type="submit" name="submit" value="Submit">
+</form>
+
+<a href="index.php">Back to home</a>
+
+<?php require "templates/footer.php"; ?>
